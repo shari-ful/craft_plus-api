67255ca24a1970f7af46db2c4f7930f5d97cab22
shari-ful <shariful.alam@intellier.com>
2023-08-01 18:23:13 +0600

    fdsfdsf

--- product/models.py
+++ product/models.py
@@ -1,19 +1,32 @@
 from django.db import models
-
+from store.models import Store
 # Create your models here.
 
 
-class Catagory(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=50, null=True)
+    icon = models.ImageField(upload_to='media/category/', blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
 class Product(models.Model):
     title = models.CharField(max_length=200, null=True)
-    price = models.FloatField(max_length=200, null=True)
+    price = models.FloatField(blank=True, null=True)
+    brand = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     discount = models.IntegerField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     digital = models.BooleanField(default=False, null=True, blank=True)
-    image_1 = models.ImageField(blank=True)
-    image_2 = models.ImageField(blank=True, null=True)
-    image_3 = models.ImageField(blank=True, null=True)
-    image_4 = models.ImageField(blank=True, null=True)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    quantity = models.IntegerField(blank=True, default=0)
+
+    image_1 = models.ImageField(upload_to='media/product/', blank=True)
+    image_2 = models.ImageField(upload_to='media/product/', blank=True, null=True)
+    image_3 = models.ImageField(upload_to='media/product/', blank=True, null=True)
+    image_4 = models.ImageField(upload_to='media/product/', blank=True, null=True)
 
     def __str__(self):
         return self.title
--- product/serializers.py
+++ product/serializers.py
@@ -1,6 +1,27 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Product
 
-class ProductSerializer(ModelSerializer):
+class GetProduct(serializers.ModelSerializer):
     class Meta:
         model = Product
+
+
+class CreateProduct(serializers.ModelSerializer):
+    title = serializers.CharField(read_only=False, required=True)
+    title = serializers.CharField(read_only=False, required=True)
+    price = serializers.FloatField(read_only=False, required=True)
+    brand = serializers.CharField(read_only=False, required=True)
+    category = serializers.ChoiceField(read_only=False, required=True)
+    discount = serializers.IntegerField(read_only=False, required=True)
+    description = serializers.CharField(read_only=False, required=True)
+    digital = serializers.BooleanField(read_only=False, required=True)
+    store = serializers.CharField(read_only=False, required=True)
+    quantity = serializers.IntegerField(read_only=False, required=True)
+    class Meta:
+        model = Product
+
+
+class UpdateProduct(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+
